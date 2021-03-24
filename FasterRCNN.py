02ef73001ad8ed88a33766c6b378256eef865553
Bilal el Attar <B.ElAttar@student.tudelft.nl>
2021-03-24 10:45:43 +0100

    Add faster RCNN class

---
+++ FasterRCNN.py
@@ -0,0 +1,100 @@
+import torch
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from torch.utils.data import DataLoader, Dataset
+
+
+# load a pre-trained model for classification and return
+# only the features
+from Reader import FruitDataset, read_data, FruitType
+from detection import utils
+from detection.engine import train_one_epoch, evaluate
+
+backbone = torchvision.models.vgg16(pretrained=False).features[:30]
+# print(backbone)
+# print(backbone)
+# FasterRCNN needs to know the number of
+# output channels in a backbone. For mobilenet_v2, it's 1280
+# so we need to add it here
+backbone.out_channels = 512
+
+# let's make the RPN generate 5 x 3 anchors per spatial
+# location, with 5 different sizes and 3 different aspect
+# ratios. We have a Tuple[Tuple[int]] because each feature
+# map could potentially have different sizes and
+# aspect ratios
+anchor_generator = AnchorGenerator(sizes=((8, 16, 32),),
+                                   aspect_ratios=((0.5, 1.0, 2.0),))
+
+# let's define what are the feature maps that we will
+# use to perform the region of interest cropping, as well as
+# the size of the crop after rescaling.
+# if your backbone returns a Tensor, featmap_names is expected to
+# be [0]. More generally, the backbone should return an
+# OrderedDict[Tensor], and in featmap_names you can choose which
+# feature maps to use.
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=["0"],
+                                                output_size=9,
+                                                sampling_ratio=2)
+
+# put the pieces together inside a FasterRCNN model
+model = FasterRCNN(backbone,
+                   num_classes=2,
+                   rpn_anchor_generator=anchor_generator,
+                   box_roi_pool=roi_pooler)
+
+def main():
+    PATH = "data/acfr-fruit-dataset/mangoes"
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # our dataset has two classes only - background and person
+    num_classes = 2
+    # use our dataset and defined transformations
+    training_data = read_data(FruitType.APPLE)[0]
+    dataset = FruitDataset(training_data, PATH)
+    dataset_test = FruitDataset(training_data, PATH)
+
+    # split the dataset in train and test set
+    # indices = torch.randperm(len(dataset)).tolist()
+    # dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    # dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    # get the model using our helper function
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
+
+main()
